e48af8841bd6be0212553c60e800fd7ca36ea927
WANNA959 <wannazjx@163.com>
2023-03-16 08:24:12 +0000

    update

---
+++ rknn-toolkit2-1.2.0/examples/caffe/mobilenet_v2/test-vgg.py
@@ -0,0 +1,78 @@
+import numpy as np
+import cv2
+from rknn.api import RKNN
+
+
+def show_outputs(outputs):
+    np.save('./vgg.npy', outputs[0])
+    output = outputs[0].reshape(-1)
+    output_sorted = sorted(output, reverse=True)
+    top5_str = 'vgg\n-----TOP 5-----\n'
+    for i in range(5):
+        value = output_sorted[i]
+        index = np.where(output == value)
+        for j in range(len(index)):
+            if (i + j) >= 5:
+                break
+            if value > 0:
+                topi = '{}: {}\n'.format(index[j], value)
+            else:
+                topi = '-1: 0.0\n'
+            top5_str += topi
+    print(top5_str)
+
+
+if __name__ == '__main__':
+
+    # Create RKNN object
+    rknn = RKNN(verbose=True)
+
+    # Pre-process config
+    print('--> Config model')
+    rknn.config(mean_values=[103.94, 116.78, 123.68], std_values=[
+                58.82, 58.82, 58.82], target_platform='rk3588', quant_img_RGB2BGR=True)
+    print('done')
+
+    # Load model
+    print('--> Loading model')
+    ret = rknn.load_caffe(model='/root/android/ncnn/build-android-aarch64/tools/caffe/deploy_new.prototxt',
+                          blobs='/root/android/ncnn/build-android-aarch64/tools/caffe/vgg_new.caffemodel')
+    if ret != 0:
+        print('Load model failed!')
+        exit(ret)
+    print('done')
+
+    # Build model
+    print('--> Building model')
+    ret = rknn.build(do_quantization=True, dataset='./dataset.txt')
+    if ret != 0:
+        print('Build model failed!')
+        exit(ret)
+    print('done')
+
+    # Export rknn model
+    print('--> Export rknn model')
+    ret = rknn.export_rknn('./vgg.rknn')
+    if ret != 0:
+        print('Export rknn model failed!')
+        exit(ret)
+    print('done')
+
+    # Set inputs
+    img = cv2.imread('./dog_224x224.jpg')
+
+    # Init runtime environment
+    print('--> Init runtime environment')
+    ret = rknn.init_runtime()
+    if ret != 0:
+        print('Init runtime environment failed!')
+        exit(ret)
+    print('done')
+
+    # Inference
+    print('--> Running model')
+    outputs = rknn.inference(inputs=[img])
+    show_outputs(outputs)
+    print('done')
+
+    rknn.release()
