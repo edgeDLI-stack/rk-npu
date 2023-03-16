b2cfa838108800a7e451c99689055868c4696bb2
WANNA959 <wannazjx@163.com>
2023-03-16 07:46:03 +0000

    update

---
+++ rknpu2/examples/rknn_resnet50v2/build/build_android_v8a/CMakeFiles/rknn_resnet50v2_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_resnet50v2_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_resnet50v2_demo.dir/src/main.cc.o.d"
+  "rknn_resnet50v2_demo"
+  "rknn_resnet50v2_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_resnet50v2_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
